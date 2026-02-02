1b3e810c0e9be1cf79fb1405639072439fdd3635
Madimetja <madimetja@digemy.com>
2026-02-02 21:28:10 +0200

    adding client args to buikd

--- loadforge/src/env.js
+++ loadforge/src/env.js
@@ -2,14 +2,9 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     DATABASE_URL: z.string().url(),
     SOCKET_URL: z.string().url(),
-    // NEXT_PUBLIC_WS_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -18,42 +13,25 @@ export const env = createEnv({
     VERCEL_URL: z.string().optional(),
   },
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
-    // NEXT_PUBLIC_APP_URL: z.string().url(),
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    // NEXT_PUBLIC_WS_URL: z.string().url(),
-    BETTER_AUTH_CALLBACK: z.string().url()
+    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_BETTER_AUTH_CALLBACK: z.string().url(),
   },
 
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
+    // Server
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     SOCKET_URL: process.env.SOCKET_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL ,
-    // VERCEL_URL: process.env.VERCEL_URL,
-    // NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    BETTER_AUTH_CALLBACK: process.env.BETTER_AUTH_CALLBACK, 
-    // NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    VERCEL_URL: process.env.VERCEL_URL,
+    
+    // Client
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_BETTER_AUTH_CALLBACK: process.env.NEXT_PUBLIC_BETTER_AUTH_CALLBACK,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
+
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
 });
